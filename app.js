d25f290a0d7d9aa94f6baace05b14f019c628fde
Zormelo-Alex <alexzormelo9@gmail.com>
2022-05-21 00:07:50 -0700

    new updates

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
+//selector
+const main = document.querySelector('.main');
+const light = document.querySelector('.main .container .light');
 const burger = document.querySelector('.header .navbar .navlist .burger');
 const mobile_menu = document.querySelector('.header .navbar .navlist ul');
 const menuitems = document.querySelectorAll('.header .navbar .navlist ul li a');
 const header = document.querySelector('.header.container');
 
+//event listeners
 burger.addEventListener('click',()=>{
     burger.classList.toggle('active');
     mobile_menu.classList.toggle('active');
@@ -13,3 +17,11 @@ menuitems.forEach((item)=>{
         mobile_menu.classList.toggle('active');
     });
 });
+
+light.addEventListener('click',white);
+
+
+//functions
+function white(){
+    main.classList.toggle('white');
+}
